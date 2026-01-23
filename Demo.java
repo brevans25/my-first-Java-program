f60f99f57e431920640532715c0f675fb5b4f9ee
brevans25 <brytnerevangelista@gmail.com>
2026-01-22 22:53:49 -0500

    Primitive Type variables explained & Visuals folder added

--- Demo.java
+++ Demo.java
@@ -1,6 +1,18 @@
 class Demo {
 
     public static void main(String []args) {
+
+    // Declare variables using the correct variable types => PRIMITIVE TYPES HERE
+        byte age = 35;
+        int viewsCount = 123456789; // or use underscore to separate large numbers like 123_456_789
+        long starsInTheSky = 3_123_456_789L; // Use suffix "L" along variable type long
+        double price = 10.99; // this variable is too large for this value
+        float priceFixed = 10.99F; // use suffix "F" along variable type float
+        char nameInitial = 'A'; // Use single quotes for single characters and double quotes for multiple characters
+        boolean isSpicy = true; // Must be lowercase (Hello, Python!)
+
+    // REFERENCE TYPES to store complex objects:
+
         System.out.println("Brytner Evangelista");
     }
 }
